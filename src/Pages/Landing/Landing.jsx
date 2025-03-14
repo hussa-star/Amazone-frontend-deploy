d3602fe603869e4f7191986510ea7893bf55c46d
Hussien <endrishussien313@gmail.com>
2025-03-14 08:03:20 +0300

    fix layout

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ImageCarousel from "../../Components/carousel/Carousel";
 import Catagory from "../../Components/Catagory/Catagory";
 import Product from "../../Components/Product/Product";
-import Layout from "../../Components/Layout/Layout";
+import Layout from "../../Components/LayOut/Layout";
 
 function Landing() {
   return (
